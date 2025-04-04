c0ced53063b990b2ada82275a2ebb48da7940bf1
KopzhasharovAzamat <kopzha01@gmail.com>
2025-04-04 15:07:05 +0600

    fix project dependencies PL: DTOs

---
+++ demo_web_api.PL/DTOs/EmployeeDto.cs
@@ -0,0 +1,9 @@
+﻿namespace demo_web_api.PL.DTOs;
+
+public class EmployeeDto {
+    public Guid   Id         { get; set; }
+    public string FirstName  { get; set; }
+    public string LastName   { get; set; }
+    public string MiddleName { get; set; }
+    public string Email      { get; set; }
+}
---
+++ demo_web_api.PL/DTOs/ProjectDto.cs
@@ -0,0 +1,12 @@
+﻿namespace demo_web_api.PL.DTOs;
+
+public class ProjectDto {
+    public Guid      Id                  { get; set; }
+    public string    Name                { get; set; }
+    public Guid      CustomerCompanyId   { get; set; }
+    public Guid      ContractorCompanyId { get; set; }
+    public DateTime  StartDate           { get; set; }
+    public DateTime? EndDate             { get; set; }
+    public int       Priority            { get; set; }
+    public Guid?     ProjectManagerId    { get; set; }
+}
